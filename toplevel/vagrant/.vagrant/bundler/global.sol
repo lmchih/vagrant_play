b9ef7a6996d0b5daf73c589b738379961a55a084
Ming-Chih Lin <ricklin126@gmail.com>
2023-09-01 11:23:57 +0800

    feat: Play with master-minion(saltstack), serverspec with Vagrant
     On branch saltstack
     Changes to be committed:
    	new file:   toplevel/salt/graphite.sls
    	new file:   toplevel/salt/top.sls
    	new file:   toplevel/salt/tuning.sls
    	new file:   toplevel/vagrant/.rspec
    	new file:   toplevel/vagrant/.vagrant/bundler/global.sol
    	new file:   toplevel/vagrant/.vagrant/rgloader/loader.rb
    	new file:   toplevel/vagrant/Rakefile
    	new file:   toplevel/vagrant/Vagrantfile
    	new file:   toplevel/vagrant/salt/key/master.pem
    	new file:   toplevel/vagrant/salt/key/master.pub
    	new file:   toplevel/vagrant/salt/key/minion.pem
    	new file:   toplevel/vagrant/salt/key/minion.pub
    	new file:   toplevel/vagrant/salt/master
    	new file:   toplevel/vagrant/salt/minion
    	new file:   toplevel/vagrant/spec/master/graphite_spec.rb
    	new file:   toplevel/vagrant/spec/spec_helper.rb

---
+++ toplevel/vagrant/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["backticks",["~> 1.0.0"]],["docker",["= 0.4.0"]],["vagrant-vmware-desktop",["= 3.0.3"]]],"checksum":"cbef1e09d36361896e5f63d401d9397a61d9ffa3d90449164bc0a98fc744e121","vagrant_version":"2.3.7"}
